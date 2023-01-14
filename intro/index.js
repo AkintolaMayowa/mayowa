9462a3b086195334a30f7166e415aab2efd2de6b
Alimazoya David <davidalimazo@gmail.com>
2023-01-14 14:40:59 +0100

    intro

---
+++ intro/index.js
@@ -0,0 +1,24 @@
+//DSA
+
+//1. null
+
+//2.undefined
+
+//3. String
+
+//4. Number
+
+//5. Object
+
+//6. Array
+
+//7. Set
+
+const array = ['david', 'mayowa', 'david']
+
+const set = new Set();
+set.add('david')
+set.add('mayowa')
+set.add('david')
+
+console.log(set)
