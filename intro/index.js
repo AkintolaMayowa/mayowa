3914d1201f769a94c3e8c10c5296c7a6fd9b230c
Alimazoya David <davidalimazo@gmail.com>
2023-01-18 14:25:23 +0100

    update

--- intro/index.js
+++ intro/index.js
@@ -14,6 +14,9 @@
 
 //7. Set
 
+//Truthy : 1, [9], 'h'
+//Falsy: 0, [], '',, null, undefined
+
 const array = ['david', 'mayowa', 'david']
 
 const set = new Set();
@@ -21,4 +24,83 @@ set.add('david')
 set.add('mayowa')
 set.add('david')
 
-console.log(set)
+//console.log(set)
+/*
+const calAmount=(price, quantity)=>{
+    return price * quantity
+}
+*/
+
+/**
+ * A function to calculate tax payable
+ * @param {number} taxRate 
+ * @param {number} amount 
+ * @returns {number}
+ */
+/*
+const calTax=(taxRate, amount)=>{
+    return taxRate * amount;
+}
+console.log(calTax(0.25, 16750));
+*/
+
+const name = 'mayowa';
+/*
+console.log(name.length);
+console.log(name.slice(0, 3))
+console.log(name.substring(0, 3))
+console.log(name.substr(0, 3))
+*/
+/*
+console.log(name.replace('m', 'b'));
+console.log(name.replaceAll('a', 'i'));
+*/
+
+/*
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+*/
+
+/*
+const firstName = 'david';
+const surname = 'alimazoya';
+
+console.log(firstName.concat(" " + surname))
+*/
+
+const sentence = " the brown fox does this and that ";
+/*
+console.log(sentence.trim());
+
+console.log(sentence.trimStart())
+console.log(sentence.trimEnd())
+console.log(sentence);
+*/
+
+/*
+console.log(sentence.padStart(37, 'all'))
+console.log(sentence.padEnd(40, 'things'))
+*/
+
+/*
+console.log(name.charAt(20));
+console.log(name.charCodeAt(1));
+
+const letter = String.fromCodePoint(65)
+console.log(letter);
+*/
+/*
+const nameArray = name.split('');
+
+const csv = 'this, is, a, comma, separated, list'
+console.log(csv.split(','))
+*/
+
+//converting to string
+const number = 45;
+//console.log(String(number))
+
+//template string
+
+const template = `my name is ${name} i stay at number ${number}`;
+console.log(template);
